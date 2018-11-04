f82de670ec7bdfe75a2c8a6b979234e5861e588d
felix <1403179190@qq.com>
2018-11-04 16:55:03 +0800

    创建博客表和标签表 添加django admin

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import BlogType, Blog
+
 
 # Register your models here.
+
+@admin.register(BlogType)
+class BlogTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type_name')  # 需要显示的列表
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'blog_type', 'author', 'created_time', 'last_updated_time')
+
+# 上面的装饰器和这句相同
+# admin.site.register(Blog,BlogAdmin)
--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+# 博客分类
+class BlogType(models.Model):
+    type_name = models.CharField(max_length=15)  # 博客分类名称
+
+    def __str__(self):  # 显示标签名
+        return self.type_name
+
+
+# 博客
+class Blog(models.Model):
+    title = models.CharField(max_length=50)  # 博客标题
+    blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING)  # 博客分类
+    content = models.TextField()  # 博客内容
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)  # 博客作者
+    created_time = models.DateTimeField(auto_now_add=True)  # 博客创建时间
+    last_updated_time = models.DateTimeField(auto_now=True)  # 博客更新事件
+
+    def __str__(self):  # 显示标题名
+        return "<Blog:{}>".format(self.title)
