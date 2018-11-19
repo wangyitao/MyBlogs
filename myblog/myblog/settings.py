29c828543892d81ec3a13a3e77013d462ddef12a
felix <1403179190@qq.com>
2018-11-19 11:27:59 +0800

    设置缓存

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -146,3 +146,12 @@ CKEDITOR_UPLOAD_PATH = 'upload/'
 
 # 自定义参数
 EACH_PAGE_BLOGS_NUMBER = 7
+
+
+# 设置数据库缓存
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'my_read_num_cache_table',
+    }
+}
