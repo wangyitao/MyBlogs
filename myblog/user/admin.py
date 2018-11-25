249acc178e1b0a5f494195de3bd8d78b65d9b3b6
felix <1403179190@qq.com>
2018-11-25 20:44:02 +0800

    自定义用户模型

--- myblog/user/admin.py
+++ myblog/user/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import Profile
+
+
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+    list_display = ('username', 'nickname', 'email', 'is_staff', 'is_active', 'is_superuser')
+
+    def nickname(self, obj):
+        return obj.profile.nikename
+
+    nickname.short_description = '昵称'  # 后台字段
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'nikename')
--- myblog/user/models.py
+++ myblog/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nikename = models.CharField(max_length=20, verbose_name='昵称',default='')
+
+    def __str__(self):
+        return '<Profile: {} for {}>'.format(self.nikename, self.user.username)
