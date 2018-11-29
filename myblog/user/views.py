347ade7ed9ca609df570e02413f56e1946efaa94
felix <1403179190@qq.com>
2018-11-29 12:01:50 +0800

    添加绑定邮箱逻辑

--- myblog/user/views.py
+++ myblog/user/views.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 # @Time    : 18-11-7 下午4:12
 # @Author  : Felix Wang
-
+import random
+import re
+import time
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib import auth
 from django.contrib.auth.models import User
 from django.urls import reverse
-from .forms import LoginForm, RegisterForm
+from django.conf import settings
+from .forms import LoginForm, RegisterForm, ChangeNicknameForm, BindEmailForm
+from .models import Profile
+from myblog.utils import AutoSendEmail
 
 
 def login(requests):
@@ -82,3 +87,109 @@ def logout(requests):
 def user_info(requests):
     context = {}
     return render(requests, 'user/user_info.html', context)
+
+
+def change_nickname(requests):
+    redirect_to = requests.GET.get('from', reverse('home'))
+
+    if requests.method == 'POST':
+        form = ChangeNicknameForm(requests.POST, user=requests.user)
+        if form.is_valid():
+            nickname_new = form.cleaned_data['nickname_new']
+            profile, created = Profile.objects.get_or_create(user=requests.user)
+            profile.nickname = nickname_new
+            profile.save()
+            return redirect(redirect_to)
+    else:
+        form = ChangeNicknameForm()
+
+    context = {
+        'submit_text': '修改',
+        'page_title': '修改昵称',
+        'form_title': '修改昵称',
+        'form': form,
+        'return_back_url': redirect_to,
+
+    }
+    return render(requests, 'form.html', context)
+
+
+def bind_email(requests):
+    redirect_to = requests.GET.get('from', reverse('home'))
+
+    if requests.method == 'POST':
+        form = BindEmailForm(requests.POST, requests=requests)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            requests.user.email = email
+            requests.user.save()
+            return redirect(redirect_to)
+    else:
+        form = BindEmailForm()
+
+    context = {
+        'submit_text': '绑定邮箱',
+        'page_title': '绑定邮箱',
+        'form_title': '绑定',
+        'form': form,
+        'return_back_url': redirect_to,
+
+    }
+    return render(requests, 'user/bind_email.html', context)
+
+
+def send_verification_code(requests):
+    email = requests.GET.get('email', '')
+    if re.match(r'^[0-9a-zA-Z_]{0,19}@[0-9a-zA-Z]{1,13}\.[com,cn,net]{1,3}$', email):
+        # 生成验证码
+        all_codes = list(range(0x30, 0x39)) + list(range(0x61, 0x74)) + list(range(0x41, 0x5a))  # 大写，小写和数字
+        code = ''.join([chr(random.choice(all_codes)) for i in range(6)])
+        now = int(time.time())
+        send_code_time = requests.session.get('send_code_time', 0)
+        if now - send_code_time < 60:
+            data = {
+                'status': 'ERROR',
+            }
+        else:
+            requests.session['bind_email_code'] = code
+            requests.session['send_code_time'] = send_code_time
+            title = '验证码'
+            auto_email = AutoSendEmail(sender=settings.EMAIL_HOST_USER, recever=[email],
+                                       password=settings.EMAIL_HOST_PASSWORD, title=title, from_who=settings.FROM_WHO,
+                                       smtp_server=settings.MAIL_HOST, port=settings.EMAIL_PORT)
+            html = """
+                        <html>
+                          <head></head>
+                          <body>
+                            <p>Hi!<br>
+                               非常感谢您绑定邮箱！
+                               <br>
+                               本次的验证码是：{}，请不要透露给其他人！
+                               <br>
+                            </p>
+                            <img style="width:180px;height:240px" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541440161574&di=fd6156e441788866ffbd6c654d75fa23&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fblog%2F201507%2F22%2F20150722222322_Ky8Nj.jpeg" />
+                          </body>
+                        </html>
+                        """.format(code)
+
+            # 以html的形式发送文字，推荐这个，因为可以添加图片等
+            auto_email.addHtml(html)
+            # 发送邮件
+            try:
+                auto_email.sendEmail()
+                data = {
+                    'status': 'SUCCESS',
+                    'msg': '邮件发送成功',
+                }
+            except Exception as e:
+                data = {
+                    'status': 'ERRORS',
+                    'msg': '邮件发送失败',
+                }
+    else:
+        data = {
+            'status': 'ERRORS',
+            'msg': '邮箱格式不正确',
+        }
+
+    return JsonResponse(data)
