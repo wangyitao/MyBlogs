f5f2db054b33ccd48282a05e406c0d01f3cb82a0
felix <1403179190@qq.com>
2018-11-29 11:59:12 +0800

    用户相关url

--- myblog/user/urls.py
+++ myblog/user/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('login/', views.login, name='login'),  # 登录
     path('logout/', views.logout, name='logout'),  # 登录
     path('login_for_model/', views.login_for_model, name='login_for_model'),  # 登录
-    path('register/', views.register, name='register'),  # 登录
-    path('user_info/', views.user_info, name='user_info'),  # 登录
+    path('register/', views.register, name='register'),  # 注册
+    path('user_info/', views.user_info, name='user_info'),  # 用户信息
+    path('change_nickname/', views.change_nickname, name='change_nickname'),  # 更改昵称
+    path('bind_email/', views.bind_email, name='bind_email'),  # 更改昵称
+    path('send_verification_code/', views.send_verification_code, name='send_verification_code'),  # 更改昵称
 ]
